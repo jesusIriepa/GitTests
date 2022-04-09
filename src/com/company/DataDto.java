b3fb2f3513c250f82eff621f45ef9f00e0e6a2e2
Jesus Iriepa <jesusiriepa@gmail.com>
2022-04-09 12:28:55 +0200

    id3 - Completed task ¡

---
+++ src/com/company/DataDto.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class DataDto {
+    public int number;
+    public String text;
+
+    @Override
+    public String toString() {
+        return "DataDto{" +
+                "number=" + number +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,67 +1,76 @@
 package com.company;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Cambio en master ¡¡¡¡¡");
-        System.out.println("Otro cambio más en master ¡¡¡¡¡");
-        Main main = new Main();
-        CompletableFuture<String> completableFuture = CompletableFuture
-                .supplyAsync(()-> main.task1())
-                .thenApplyAsync(t1-> main.task2(t1))
-                .thenApplyAsync(t1t2-> main.task4(t1t2))
-                .thenApplyAsync(t2t3 -> main.task3(t2t3))
-                        .handle((tx, ex) -> {
-                            ex.printStackTrace();
-                            return "exception";
-                        });
-        System.out.println("Segundo commit");
-        System.out.println(completableFuture.get());
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Añadido en el tercer commit");
+    Main main = new Main();
+    CompletableFuture<DataDto> completableFuture =
+        CompletableFuture.supplyAsync(() -> main.task1())
+            .thenApplyAsync(t1 -> main.task2(t1))
+            .thenApplyAsync(t1t2 -> main.task4(t1t2))
+            .thenApplyAsync(t2t3 -> main.task3(t2t3))
+            .thenApplyAsync(ret -> main.task5(ret))
+            .handle(
+                (tx, ex) -> {
+                  if (ex != null) {
+                    ex.printStackTrace();
+                  }
+                  return tx;
+                });
+    System.out.println("Segundo commit");
+    System.out.println(completableFuture.get());
+  }
 
-    String task1() {
-        for(int i=0; i<10; i++){
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getId() + " task 1 - " + i);
-        }
-        System.out.println("Tercer commit");
-        return "task1";
+  String task1() {
+    for (int i = 0; i < 10; i++) {
+      try {
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println(Thread.currentThread().getId() + " task 1 - " + i);
     }
+    System.out.println("Tercer commit");
+    return "task1";
+  }
 
-    String task2(String t1) {
-        for(int i=0; i<10; i++){
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getId() + " task 2- " + i);
-            throw new RuntimeException("task2 ha petao");
-        }
-        return t1 + "task2";
+  String task2(String t1) {
+    for (int i = 0; i < 10; i++) {
+      try {
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println(Thread.currentThread().getId() + " task 2- " + i);
     }
+    return t1 + "task2";
+  }
 
-    String task3(String t1t2) {
-        for(int i=0; i<10; i++){
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getId() + " task 3- " + i);
-        }
-        return t1t2 + "task3";
+  String task3(String t1t2) {
+    for (int i = 0; i < 10; i++) {
+      try {
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println(Thread.currentThread().getId() + " task 3- " + i);
     }
+    return t1t2 + "task3";
+  }
 
-    String task4(String text){
-        System.out.println("received: " + text);
-        return text + " taks4 modified";
-    }
+  String task4(String text) {
+    System.out.println("received: " + text);
+    return text + " taks4 modified";
+  }
 
+  DataDto task5(String text) {
+    DataDto dataDto = new DataDto();
+    dataDto.text = text;
+    dataDto.number = new Random().nextInt();
+    return dataDto;
+  }
 }
