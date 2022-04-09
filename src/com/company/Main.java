ac926fb8e19515eb4e5826d7d9c80e66997d1936
Jesus Iriepa <jesusiriepa@gmail.com>
2022-04-09 11:20:15 +0200

    id1 - Changes in Main Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,18 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-
+        System.out.println("Añadido en el tercer commit");
         Main main = new Main();
         CompletableFuture<String> completableFuture = CompletableFuture
                 .supplyAsync(()-> main.task1())
                 .thenApplyAsync(t1-> main.task2(t1))
+                .thenApplyAsync(t1t2-> main.task4(t1t2))
                 .thenApplyAsync(t2t3 -> main.task3(t2t3))
                         .handle((tx, ex) -> {
                             ex.printStackTrace();
                             return "exception";
                         });
-
+        System.out.println("Segundo commit");
         System.out.println(completableFuture.get());
     }
 
@@ -29,6 +29,7 @@ public class Main {
             }
             System.out.println(Thread.currentThread().getId() + " task 1 - " + i);
         }
+        System.out.println("Tercer commit");
         return "task1";
     }
 
@@ -45,7 +46,7 @@ public class Main {
         return t1 + "task2";
     }
 
-    String  task3(String t1t2) {
+    String task3(String t1t2) {
         for(int i=0; i<10; i++){
             try {
                 Thread.sleep(200);
@@ -57,5 +58,9 @@ public class Main {
         return t1t2 + "task3";
     }
 
+    String task4(String text){
+        System.out.println("received: " + text);
+        return text + " taks4 modified";
+    }
 
 }
